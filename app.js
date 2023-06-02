b1a36ac3d21a677196fafe95372191d75be5e948
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-06-02 13:42:16 +0530

    modified the code using route handelers

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 //body-parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //conection
 mongoose.connect("mongodb://0.0.0.0:27017/wikiDB", { useNewUrlParser: true });
@@ -24,47 +24,45 @@ const articleSchema = mongoose.Schema({
 const Article = mongoose.model("Article", articleSchema);
 
 //route for home route
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("Your app is healty and running on the port " + port);
 })
 
-//use of GET method to find all the articles to fetch the atricles from Robo 3T
-app.get("/articles", (req, res)=>{
-    Article.find((err, foundArticles)=>{
-        if(err){
-            res.send(err);
-        } else{
-            res.send(foundArticles);
-        }
+//route handelers
+app.route("/articles").
+    get((req, res) => {
+        Article.find((err, foundArticles) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(foundArticles);
+            }
+        });
+    })
+    .post((req, res) => {
+        const newArticle = new Article({
+            tittle: req.body.tittle,
+            content: req.body.content
+        });
+        newArticle.save((err) => {
+            if (!err) {
+                res.send("successfully enterd the data");
+            } else {
+                res.send("Oops error found");
+            }
+        });
+    })
+    .delete((req, res) => {
+        Article.deleteMany((err) => {
+            if (!err) {
+                res.send("Successfully deteled all the data");
+            } else {
+                res.send(err);
+            }
+        });
     });
-});
 
-//use of POST method to create a data in the database
-app.post("/articles", (req, res)=>{
-    const newArticle = new Article({
-        tittle: req.body.tittle,
-        content: req.body.content
-    });
-    newArticle.save((err)=>{
-        if(!err){
-            res.send("successfully enterd the data");
-        } else{
-            res.send("Oops error found");
-        }
-    });
-});
 
-//use of delete method to delete all the article
-app.delete("/articles", (req, res)=>{
-    Article.deleteMany((err)=>{
-        if(!err){
-            res.send("Successfully deteled all the data");
-        } else{
-            res.send(err);
-        }
-    });
-});
-
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("Your app is listening on the port " + port);
 })
